c615043fc4492beeba64b75cf1c31b2845cc70f0
xanimal37 <hawkbit37@gmail.com>
2023-04-18 12:21:06 -0500

    security config

--- Barter/src/main/java/com/skilldistillery/barter/BarterApplication.java
+++ Barter/src/main/java/com/skilldistillery/barter/BarterApplication.java
@@ -2,12 +2,23 @@ package com.skilldistillery.barter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BarterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarterApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
