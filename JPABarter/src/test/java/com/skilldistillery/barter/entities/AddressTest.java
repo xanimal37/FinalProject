2f577cc26e7290d5c82874612dbe0a27ebfc9791
xanimal37 <hawkbit37@gmail.com>
2023-04-19 15:20:25 -0500

    Address, Task, entity mapping

---
+++ JPABarter/src/test/java/com/skilldistillery/barter/entities/AddressTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.barter.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AddressTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPABarter");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		address=null;
+		em.close();
+	}
+
+	@Test
+	void test_Address_mapping() {
+		assertNotNull(address);
+		assertEquals("123 Main St", address.getStreet1());
+	}
+
+}
