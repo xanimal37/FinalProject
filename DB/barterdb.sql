088045b2ee10184a1d4bd2b8dae5fcd97a86835d
rob.tisdale <rtisdale.bt@gmail.com>
2023-04-19 12:48:15 -0500

    ERDiagram added

--- DB/barterdb.sql
+++ DB/barterdb.sql
@@ -16,6 +16,35 @@ CREATE SCHEMA IF NOT EXISTS `barterdb` DEFAULT CHARACTER SET utf8 ;
 USE `barterdb` ;
 
 -- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `street1` VARCHAR(45) NULL,
+  `street2` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(2) NULL,
+  `zipcode` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ranking`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `ranking` ;
+
+CREATE TABLE IF NOT EXISTS `ranking` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
@@ -26,8 +55,368 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` VARCHAR(150) NOT NULL,
   `enabled` TINYINT NOT NULL,
   `role` VARCHAR(45) NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `email` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  `create_date` DATETIME NULL,
+  `update_date` DATETIME NULL,
+  `firstname` VARCHAR(45) NULL,
+  `lastname` VARCHAR(45) NULL,
+  `address_id` INT NULL,
+  `ranking_id` INT NULL,
+  `biography` TEXT NULL,
+  `availability` VARCHAR(500) NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  INDEX `fk_user_address1_idx` (`address_id` ASC),
+  INDEX `fk_user_ranking1_idx` (`ranking_id` ASC),
+  CONSTRAINT `fk_user_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_ranking1`
+    FOREIGN KEY (`ranking_id`)
+    REFERENCES `ranking` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `task_status`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `task_status` ;
+
+CREATE TABLE IF NOT EXISTS `task_status` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `task`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `task` ;
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` TEXT NULL,
+  `estimated_hours` INT NULL,
+  `materials_provided` TINYINT NULL,
+  `schedule_date` DATETIME NULL,
+  `start_date` DATETIME NULL,
+  `complete_date` DATETIME NULL,
+  `create_date` DATETIME NULL,
+  `update_date` DATETIME NULL,
+  `task_status_id` INT NOT NULL,
+  `address_id` INT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_task_task_status1_idx` (`task_status_id` ASC),
+  INDEX `fk_task_address1_idx` (`address_id` ASC),
+  INDEX `fk_task_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_task_task_status1`
+    FOREIGN KEY (`task_status_id`)
+    REFERENCES `task_status` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_task_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_task_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `skill`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `skill` ;
+
+CREATE TABLE IF NOT EXISTS `skill` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `skill_level`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `skill_level` ;
+
+CREATE TABLE IF NOT EXISTS `skill_level` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `complaint`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `complaint` ;
+
+CREATE TABLE IF NOT EXISTS `complaint` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL,
+  `create_date` DATETIME NULL,
+  `update_date` DATETIME NULL,
+  `closed_date` DATETIME NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_complaint_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_complaint_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `accepted_task`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `accepted_task` ;
+
+CREATE TABLE IF NOT EXISTS `accepted_task` (
+  `acceptor_id` INT NOT NULL,
+  `task_id` INT NOT NULL,
+  `rating_by_requester` INT NULL,
+  `rating_by_acceptor` INT NULL,
+  `remarks_by_requester` TEXT NULL,
+  `remarks_by_acceptor` TEXT NULL,
+  `accept_date` DATETIME NULL,
+  PRIMARY KEY (`acceptor_id`, `task_id`),
+  INDEX `fk_user_has_task_task2_idx` (`task_id` ASC),
+  INDEX `fk_user_has_task_user1_idx` (`acceptor_id` ASC),
+  CONSTRAINT `fk_user_has_task_user1`
+    FOREIGN KEY (`acceptor_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_task_task2`
+    FOREIGN KEY (`task_id`)
+    REFERENCES `task` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `post`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `post` ;
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `content` TEXT NOT NULL,
+  `user_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  `update_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_post_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_post_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `comment` ;
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `content` TEXT NULL,
+  `post_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `in_reply_to_id` INT NULL,
+  `create_date` DATETIME NULL,
+  `update_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comment_post1_idx` (`post_id` ASC),
+  INDEX `fk_comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_comment_comment1_idx` (`in_reply_to_id` ASC),
+  CONSTRAINT `fk_comment_post1`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `post` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_comment1`
+    FOREIGN KEY (`in_reply_to_id`)
+    REFERENCES `comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_has_friends`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_has_friends` ;
+
+CREATE TABLE IF NOT EXISTS `user_has_friends` (
+  `user_id` INT NOT NULL,
+  `friend_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `friend_id`),
+  INDEX `fk_user_has_user_user2_idx` (`friend_id` ASC),
+  INDEX `fk_user_has_user_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_user_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_user_user2`
+    FOREIGN KEY (`friend_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `notification_type`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `notification_type` ;
+
+CREATE TABLE IF NOT EXISTS `notification_type` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `notification`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `notification` ;
+
+CREATE TABLE IF NOT EXISTS `notification` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `message` VARCHAR(45) NULL,
+  `user_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  `notification_type_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_notification_user1_idx` (`user_id` ASC),
+  INDEX `fk_notification_notification_type1_idx` (`notification_type_id` ASC),
+  CONSTRAINT `fk_notification_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_notification_notification_type1`
+    FOREIGN KEY (`notification_type_id`)
+    REFERENCES `notification_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_skill`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_skill` ;
+
+CREATE TABLE IF NOT EXISTS `user_skill` (
+  `user_id` INT NOT NULL,
+  `skill_id` INT NOT NULL,
+  `skill_level_id` INT NOT NULL,
+  `certification` VARCHAR(45) NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`user_id`, `skill_id`),
+  INDEX `fk_user_has_skill_skill1_idx` (`skill_id` ASC),
+  INDEX `fk_user_has_skill_user1_idx` (`user_id` ASC),
+  INDEX `fk_user_has_skill_skill_level1_idx` (`skill_level_id` ASC),
+  CONSTRAINT `fk_user_has_skill_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_skill_skill1`
+    FOREIGN KEY (`skill_id`)
+    REFERENCES `skill` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_skill_skill_level1`
+    FOREIGN KEY (`skill_level_id`)
+    REFERENCES `skill_level` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `task_has_skill`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `task_has_skill` ;
+
+CREATE TABLE IF NOT EXISTS `task_has_skill` (
+  `task_id` INT NOT NULL,
+  `skill_id` INT NOT NULL,
+  PRIMARY KEY (`task_id`, `skill_id`),
+  INDEX `fk_task_has_skill_skill1_idx` (`skill_id` ASC),
+  INDEX `fk_task_has_skill_task1_idx` (`task_id` ASC),
+  CONSTRAINT `fk_task_has_skill_task1`
+    FOREIGN KEY (`task_id`)
+    REFERENCES `task` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_task_has_skill_skill1`
+    FOREIGN KEY (`skill_id`)
+    REFERENCES `skill` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `task_message`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `task_message` ;
+
+CREATE TABLE IF NOT EXISTS `task_message` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NULL,
+  `content` TEXT NULL,
+  `create_date` DATETIME NULL,
+  `task_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_task_message_task1_idx` (`task_id` ASC),
+  INDEX `fk_task_message_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_task_message_task1`
+    FOREIGN KEY (`task_id`)
+    REFERENCES `task` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_task_message_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -46,7 +435,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `barterdb`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'admin', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, 'admin');
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `image_url`, `email`, `phone`, `create_date`, `update_date`, `firstname`, `lastname`, `address_id`, `ranking_id`, `biography`, `availability`) VALUES (1, 'admin', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, 'admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 COMMIT;
 
