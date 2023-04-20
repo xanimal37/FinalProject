d9782bb9155c67a8a21dacae480c79dd72ef509d
Edwin <Flattoedwin@gmail.com>
2023-04-20 12:09:24 -0400

    user repo stub

--- Barter/src/main/java/com/skilldistillery/barter/services/UserService.java
+++ Barter/src/main/java/com/skilldistillery/barter/services/UserService.java
@@ -1,13 +1,14 @@
 package com.skilldistillery.barter.services;
 
-import java.util.List;
+import java.util.Set;
 
 import com.skilldistillery.barter.entities.User;
 
 public interface UserService {
-	public List <User> getAllUsers();
+	public Set <User> getAllUsers();
 	public User updateUser(User user,int userId);
 	public String changePassword(String username, String currentPassword, String newPassword);
 	public User archiveUser(int id);
 	public void deleteUser(int userId);
+	public User unArchiveUser(int id);
 }
