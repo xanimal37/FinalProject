ae65cf21988fd909d18f0f878253d1d63e6fa730
rob.tisdale <rtisdale.bt@gmail.com>
2023-04-21 14:42:25 -0500

    stubbing ng models,services,components

--- ngBarter/src/app/app-routing.module.ts
+++ ngBarter/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PostComponent } from './components/post/post.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'userAccount', component: UserAccountComponent },
+   { path: 'post', component: PostComponent},
+   { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{useHash:true})],
--- ngBarter/src/app/app.module.ts
+++ ngBarter/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PostComponent } from './components/post/post.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ComplaintComponent } from './components/complaint/complaint.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    UserAccountComponent,
+    PageNotFoundComponent,
+    ComplaintComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule,
