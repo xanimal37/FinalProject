204ee97ca165958240c552ad689832bd7189c5ba
rob.tisdale <rtisdale.bt@gmail.com>
2023-04-22 14:52:14 -0500

    partial work to complaints complete

--- ngBarter/src/app/components/complaint/complaint.component.ts
+++ ngBarter/src/app/components/complaint/complaint.component.ts
@@ -1,4 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Complaint } from 'src/app/models/complaint';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { ComplaintService } from 'src/app/services/complaint.service';
 
 @Component({
   selector: 'app-complaint',
@@ -6,5 +12,120 @@ import { Component } from '@angular/core';
   styleUrls: ['./complaint.component.css']
 })
 export class ComplaintComponent {
+
+  title: string = "Complaints";
+  complaint: Complaint | null = null;
+  selected: Complaint | null =  null;
+  selectedComplaint: Complaint[] | null = null;
+  newComplaint: Complaint = new Complaint;
+  editComplaint: Complaint | null = null;
+  complaints: Complaint[] = []
+  showEnabled: boolean = false;
+  loggedInUser: User | undefined;
+  admin: string = "admin"
+
+
+  constructor(
+    private complaintService: ComplaintService,
+    private datePipe: DatePipe,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
+    ) {
+    }
+
+  displayPost(complaint: Complaint) {
+    this.selected = complaint;
+  }
+
+  displayTable():void {
+    this.selected = null;
+  }
+
+  setEditComplaint(complaint: Complaint): void {
+    this.editComplaint = complaint;
+  }
+  cancelEditComplaint(): void {
+    this.editComplaint = null;
+  }
+
+  ngOnInit(): void {
+    this.authService.getLoggedInUser().subscribe({
+      next: (user: User) => {
+        this.loggedInUser = user;
+      },
+      error: (nojoy) => {
+        console.log(nojoy);
+      }
+
+     });
+    this.reload();
+   }
+
+   reload() {
+    this.complaintService.indexAll().subscribe({
+      next: (complaintList) => {
+        this.complaints = complaintList;
+      },
+      error: (err) => {
+        console.error('Error getting complaints list');
+        console.error(err);
+      }
+    });
+  }
+
+  getComplaintsByUser(uId: number) {
+    this.complaintService.indexComplaintsByUser(uId).subscribe({
+      next: (complaintUserList) => {
+        this.complaints = complaintUserList;
+      },
+      error: (err) => {
+        console.error('Error getting complaints list');
+        console.error(err);
+      }
+    });
+  }
+
+  getSingleComplaint(cId: number) {
+    this.complaintService.indexByComplaintId(cId).subscribe({
+      next: (singleComplaint) => {
+        this.complaint = singleComplaint;
+      },
+      error: (err) => {
+        console.error('Error getting complaints list');
+        console.error(err);
+      }
+    });
+  }
+
+  createPost(complaint: Complaint) {
+    this.complaintService.createComplaint(complaint).subscribe( {
+      next: (createdComplaint) => {
+        this.newComplaint = createdComplaint;
+        this.reload();
+      },
+      error: (fail) => {
+        console.error('Error creating complaint');
+        console.error(fail);
+      }
+    });
+
+    this.reload();
+  }
+
+  updatePost(complaint: Complaint, cId: number) {
+    this.complaintService.updateComplaint(complaint,cId).subscribe( {
+      next: (updatedPost) => {
+        this.editComplaint = updatedPost;
+        this.reload();
+      },
+      error: (fail) => {
+        console.error('Error editing post');
+        console.error(fail);
+      }
+    });
+    this.reload();
+  }
+
 
 }
--- ngBarter/src/app/services/complaint.service.ts
+++ ngBarter/src/app/services/complaint.service.ts
@@ -4,7 +4,6 @@ import { environment } from 'src/environments/environment';
 import { AuthService } from './auth.service';
 import { Complaint } from '../models/complaint';
 import { Observable, catchError, throwError } from 'rxjs';
-import { Post } from '../models/post';
 
 @Injectable({
   providedIn: 'root'
@@ -28,72 +27,72 @@ export class ComplaintService {
     return options;
   }
 
-    //All posts
-    indexAll(): Observable<Post[]> {
-      return this.http.get<Post[]>(this.url, this.getHttpOptions()).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError(
-            () => new Error('PostService.indexAll(): error retrieving Posts: ' + err)
-          );
-        })
-      );
-    };
 
-  //Post by Post Id
-    indexByPostId(pId: number): Observable<Post> {
-      return this.http.get<Post>(this.url + "/" + pId, this.getHttpOptions()).pipe(
+    indexAll(): Observable<Complaint[]> {
+      return this.http.get<Complaint[]>(this.url, this.getHttpOptions()).pipe(
         catchError((err: any) => {
           console.log(err);
           return throwError(
-            () => new Error('PostService.index(): error retrieving Post: ' + err)
-          );
-        })
-      );
-    };
-
-  //All posts by user
-    indexByUser(uId: number): Observable<Post[]> {
-      return this.http.get<Post[]>(this.url + "/user/" + uId, this.getHttpOptions()).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError(
-            () => new Error('PostService.index(): error retrieving Posts: ' + err)
+            () => new Error('ComplaintService.indexAll(): error retrieving complaints: ' + err)
           );
         })
       );
     };
 
 
-    create(complaint: Complaint):Observable<Complaint> {
+    indexByComplaintId(cId: number): Observable<Complaint> {
+      return this.http.get<Complaint>(this.url + "/" + cId, this.getHttpOptions()).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () => new Error('ComplaintService.indexByComplaintId(): error retrieving complaint: ' + err)
+          );
+        })
+      );
+    };
+
+
+    indexComplaintsByUser(uId: number): Observable<Complaint[]> {
+      return this.http.get<Complaint[]>(this.url + "/user/" + uId, this.getHttpOptions()).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () => new Error('ComplaintService.indexComplaintsByUser(): error retrieving complaints: ' + err)
+          );
+        })
+      );
+    };
+
+
+    createComplaint(complaint: Complaint):Observable<Complaint> {
       return this.http.post<Complaint>(this.url, complaint, this.getHttpOptions()).pipe(
         catchError((err: any) => {
           console.log(err);
           return throwError(
-            () => new Error('PostService.index(): error retrieving Post: ' + err)
+            () => new Error('ComplaintService.create: error creating complaint: ' + err)
           );
         })
       );
     };
 
 
-    update(complaint: Complaint, id: number):Observable<Complaint> {
-      return this.http.put<Complaint>(this.url + "/" + id, complaint, this.getHttpOptions()).pipe(
+    updateComplaint(complaint: Complaint, cid: number):Observable<Complaint> {
+      return this.http.put<Complaint>(this.url + "/" + cid, complaint, this.getHttpOptions()).pipe(
         catchError((err: any) => {
           console.log(err);
           return throwError(
-            () => new Error('PostService.index(): error updating complaing: ' + err)
+            () => new Error('ComplaintService.updateComplaint(): error updating complaint: ' + err)
           );
         })
       );
     };
 
-    archive(id: Number): Observable<void> {
-      return this.http.put<void>(this.url + "/archive/" + id, this.getHttpOptions()). pipe(
+    archiveComplaint(cid: Number): Observable<void> {
+      return this.http.put<void>(this.url + "/archive/" + cid, this.getHttpOptions()). pipe(
         catchError((err: any) => {
           console.log(err);
           return throwError(
-            () => new Error('TodoService.create(): error deletingtodo: ' + err)
+            () => new Error('ComplaintService.archiveComplaint(): error archiving complaint' + err)
           );
         })
       )
