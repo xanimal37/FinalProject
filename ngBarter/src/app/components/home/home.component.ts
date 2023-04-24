4bcdc9eb608069c0ebe32431ea694f13bf7e593c
rob.tisdale <rtisdale.bt@gmail.com>
2023-04-24 09:32:36 -0500

    UI charts with dummy data added

--- ngBarter/src/app/components/home/home.component.ts
+++ ngBarter/src/app/components/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   loggedInUser: User = new User();
   skill: Userskill | undefined;
 
-  chartOptions = {
+  skillsAvailable = {
 	  animationEnabled: true,
 	  theme: "dark2",
 	  exportEnabled: true,
@@ -26,17 +26,50 @@ export class HomeComponent implements OnInit {
 		text: "Percentage of users with skill"
 	  }],
 	  data: [{
-		type: "pie", //change type to column, line, area, doughnut, etc
+		type: "doughnut", //change type to column, line, area, doughnut, etc
 		indexLabel: "{name}: {y}%",
 		dataPoints: [
-			{ name: "Overhead", y: 9.1 },
-			{ name: "Problem Solving", y: 3.7 },
-			{ name: "Debugging", y: 36.4 },
-			{ name: "Writing Code", y: 30.7 },
-			{ name: "Firefighting", y: 20.1 }
+			{ name: "Carpentry", y: 9.1 },
+			{ name: "Plumbing", y: 3.7 },
+			{ name: "IT/Networking", y: 26.4 },
+			{ name: "LandScaping", y: 19.6 },
+			{ name: "Gardening", y: 17.3 },
+			{ name: "Welding", y: 5.0 },
+			{ name: "Packing/Moving", y: 18.9 }
 		]
 	  }]
 	}
+
+  numberOfTasks = {
+	  animationEnabled: true,
+	  theme: "dark2",
+	  exportEnabled: true,
+	  title: {
+		text: "Tasks Completed"
+	  },
+	  subtitles: [{
+		text: "Number of tasks completed by users this year"
+	  }],
+	  data: [{
+		type: "line", //change type to column, line, area, doughnut, etc
+		indexLabel: "{name}: {y}%",
+		dataPoints: [
+      { name: "May", y: 15 },
+			{ name: "June", y: 16 },
+			{ name: "July", y: 12 },
+			{ name: "August", y: 13 },
+			{ name: "September", y: 19 },
+			{ name: "October", y: 14 },
+			{ name: "November", y: 10 },
+			{ name: "December", y: 4},
+			{ name: "January", y: 4 },
+			{ name: "February", y: 6 },
+			{ name: "March", y: 26.7},
+			{ name: "April", y: 19.10 }
+		]
+	  }]
+	}
+
 
   constructor(
     private route: ActivatedRoute,
--- ngBarter/src/app/models/comment.ts
+++ ngBarter/src/app/models/comment.ts
@@ -1,2 +1,29 @@
+import { Post } from "./post";
+import { User } from "./user";
+
 export class Comment {
+  id: number;
+  content: string;
+  post: Post | null;
+  user: User | null;
+  createDate: string;
+  updateDate: string;
+
+  constructor(
+    id: number = 0,
+    content: string = '',
+    post: Post | null = null,
+    user: User  | null = null,
+    createDate: string = '',
+    updateDate: string = '',
+  ){
+    this.id = id;
+    this.content = content;
+    this.post = post;
+    this.user = user;
+    this.createDate = createDate;
+    this.updateDate = updateDate;
+
+  }
+
 }
